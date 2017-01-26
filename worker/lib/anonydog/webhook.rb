a25a4323b4ced314d1f5f3070873add4abf4d334
Thiago Arrais <thiago.arrais@gmail.com>
2017-01-26 17:57:19 +0000

    Plug webhook into anonymizing code
    Branch produced by this code: https://github.com/arraisbot/auto-reply/commits/pullrequest-e8c75570
    
    Based on PR: https://github.com/arraisbot/auto-reply/pull/2

--- worker/lib/anonydog/webhook.rb
+++ worker/lib/anonydog/webhook.rb
@@ -9,18 +9,35 @@ module Anonydog
     end
 
     post '/_github/webhook' do
+      content_type :txt
+
       puts "received webhook event"
 
       event = JSON.parse(request.body.read)
 
       puts "opened? => #{event["action"]}"
+      
+      return "ignored" if !is_open_pull_request(event)
+      
+      base_clone_url = event["pull_request"]["base"]["repo"]["clone_url"]
+      base_commit = event["pull_request"]["base"]["sha"]
+      head_clone_url = event["pull_request"]["head"]["repo"]["clone_url"]
+      head_commit = event["pull_request"]["head"]["sha"]
+      publish_url = event["pull_request"]["base"]["repo"]["ssh_url"]
 
-      puts %{base_clone_url: #{event["pull_request"]["base"]["repo"]["clone_url"]}}
-      puts %{base_commit: #{event["pull_request"]["base"]["sha"]}}
-      puts %{head_clone_url: #{event["pull_request"]["head"]["repo"]["clone_url"]}}
-      puts %{head_commit: #{event["pull_request"]["head"]["sha"]}}
-      puts %{publish_url: #{event["pull_request"]["base"]["repo"]["ssh_url"]}}
+      anonref = Anonydog::Local.publish_anonymized(
+        base_clone_url, base_commit,
+        head_clone_url, head_commit,
+        publish_url,
+      )
 
+      msg = "anonymized commits are at #{anonref}"
+      puts msg
+      msg
+    end
+    
+    def is_open_pull_request(event)
+      "opened" == event["action"] && !event["pull_request"].nil?
     end
   end
 end
