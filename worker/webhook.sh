1569c27dfa7e6e2058794012284a2fa7dc75ac15
Thiago Arrais <thiago.arrais@gmail.com>
2017-02-03 23:32:23 +0000

    Remember to set access token

--- worker/webhook.sh
+++ worker/webhook.sh
@@ -1,1 +1,5 @@
-bundle exec rackup -s thin -o $IP -p $PORT
+if [ -z ${GITHUB_API_ACCESS_TOKEN+x} ]; then
+  echo "need GITHUB_API_ACCESS_TOKEN"
+  exit
+fi
+bundle exec rackup -s thin -o $IP -p $PORT webhook.config.ru
