94b1fb52093c153a938a7fa6878b341c6be7071f
Thiago Arrais <thiago.arrais@gmail.com>
2017-03-25 00:25:43 +0000

    Repo isn't forked instanly. Call to `create_hook` may fail for some time.
    Reference: https://developer.github.com/v3/repos/forks/#create-a-fork

--- worker/lib/anonydog/webapp.rb
+++ worker/lib/anonydog/webapp.rb
@@ -10,21 +10,34 @@ module Anonydog
 
       github_api = Octokit::Client.new(access_token: ENV['GITHUB_API_ACCESS_TOKEN'])
       forked_repo = github_api.fork("#{user}/#{repo}")
-
-      github_api.create_hook(forked_repo.full_name,
-        "web",
-        {
-          url: ENV['GITHUB_WEBHOOK_ENDPOINT'],
-          secret: ENV['GITHUB_WEBHOOK_SECRET'],
-          content_type: 'json'
-        },
-        {
-          events: ['pull_request'],
-          active: true
-        }
-      )
+      do_create_hook(forked_repo.full_name, 100)
 
       "ok"
+    end
+
+    def do_create_hook(repo_name, wait)
+      begin
+        github_api.create_hook(forked_repo.full_name,
+          "web",
+          {
+            url: ENV['GITHUB_WEBHOOK_ENDPOINT'],
+            secret: ENV['GITHUB_WEBHOOK_SECRET'],
+            content_type: 'json'
+          },
+          {
+            events: ['pull_request'],
+            active: true
+          }
+        )
+      rescue Octokit::NotFound
+        if wait > 60000 then # we waited for more than a minute (60000 < 100 + 200 + 400 + 800 + 1600 + 3200 + 6400 + 12800 + 25600 + 51200)
+          puts "after waiting for more than a minute, we still do not have a working repo. panic!"
+          return
+        end
+        puts "Github did not create the fork yet. Trying again in #{wait}ms..."
+        sleep wait
+        do_create_hook(forked_repo.full_name, wait * 2)
+      end
     end
 
     get "/fork" do
