4c65416250882295d4a43ee4a228181617d31932
Thiago Arrais <thiago.arrais@gmail.com>
2017-04-06 23:04:22 +0000

    can't add commits to the anonymized PR just yet...

--- worker/lib/anonydog/webhook/webhook.rb
+++ worker/lib/anonydog/webhook/webhook.rb
@@ -19,27 +19,35 @@ module Anonydog
       verify_signature(payload_body)
 
       event = JSON.parse(payload_body)
+      
+      puts event["action"]
 
-      return "ignored" if !is_open_pull_request(event)
+      return "ignored" if !is_interested_in(event)
 
-      pull_request = {}
-      pull_request[:url] = event["pull_request"]["url"]
-      pull_request[:title] = event["pull_request"]["title"]
-      pull_request[:body] = event["pull_request"]["body"]
-      pull_request[:comments_url] = event["pull_request"]["comments_url"]
-
-      pull_request[:base] = {}
-      pull_request[:base][:clone_url] = event["pull_request"]["base"]["repo"]["clone_url"]
-      pull_request[:base][:commit_sha] = event["pull_request"]["base"]["sha"]
-      pull_request[:base][:ssh_url] = event["pull_request"]["base"]["repo"]["ssh_url"]
-      pull_request[:base][:owner_login] = event["pull_request"]["base"]["repo"]["owner"]["login"]
-      pull_request[:base][:repo_full_name] = event["pull_request"]["base"]["repo"]["full_name"]
-
-      pull_request[:head] = {}
-      pull_request[:head][:clone_url] = event["pull_request"]["head"]["repo"]["clone_url"]
-      pull_request[:head][:commit_sha] = event["pull_request"]["head"]["sha"]
-
-      do_anonymize(pull_request)
+      if is_open_pull_request(event) then
+        pull_request = {}
+        pull_request[:url] = event["pull_request"]["url"]
+        pull_request[:title] = event["pull_request"]["title"]
+        pull_request[:body] = event["pull_request"]["body"]
+        pull_request[:comments_url] = event["pull_request"]["comments_url"]
+  
+        pull_request[:base] = {}
+        pull_request[:base][:clone_url] = event["pull_request"]["base"]["repo"]["clone_url"]
+        pull_request[:base][:commit_sha] = event["pull_request"]["base"]["sha"]
+        pull_request[:base][:ssh_url] = event["pull_request"]["base"]["repo"]["ssh_url"]
+        pull_request[:base][:owner_login] = event["pull_request"]["base"]["repo"]["owner"]["login"]
+        pull_request[:base][:repo_full_name] = event["pull_request"]["base"]["repo"]["full_name"]
+  
+        pull_request[:head] = {}
+        pull_request[:head][:clone_url] = event["pull_request"]["head"]["repo"]["clone_url"]
+        pull_request[:head][:commit_sha] = event["pull_request"]["head"]["sha"]
+  
+        do_anonymize(pull_request)
+      elsif is_synchronize_pull_request(event) then
+        pull_request = {}
+        pull_request[:comments_url] = event["pull_request"]["comments_url"]
+        do_synchronize(pull_request)
+      end
     end
 
     def verify_signature(payload_body)
@@ -47,8 +55,21 @@ module Anonydog
       return halt 400, "Signature doesn't check. Are you Github?" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
     end
 
+    def is_interested_in(event)
+      is_open_pull_request(event) ||
+      is_synchronize_pull_request(event)
+    end
+
     def is_open_pull_request(event)
       "opened" == event["action"] && !event["pull_request"].nil?
+    end
+
+    def is_synchronize_pull_request(event)
+      "synchronize" == event["action"] && !event["pull_request"].nil?
+    end
+    
+    def github_api
+      @github_api ||= Octokit::Client.new(access_token: ENV['GITHUB_API_ACCESS_TOKEN'])
     end
 
     def do_anonymize(pull_request)
@@ -57,8 +78,6 @@ module Anonydog
         pull_request[:head][:clone_url], pull_request[:head][:commit_sha],
         pull_request[:base][:ssh_url]
       )
-
-      github_api = Octokit::Client.new(access_token: ENV['GITHUB_API_ACCESS_TOKEN'])
 
       bot_repo = github_api.repository(pull_request[:base][:repo_full_name])
       original_repo_name = bot_repo.parent["full_name"]
@@ -83,6 +102,16 @@ module Anonydog
 
       github_api.post(pull_request[:comments_url], body: msg)
 
+      msg
+    end
+    
+    def do_synchronize(pull_request)
+      msg_template = "sorry_no_additions.md"
+      msg_context = {}
+      msg = Messages.render_file(msg_template, msg_context)
+
+      github_api.post(pull_request[:comments_url], body: msg)
+      
       msg
     end
   end
