a3da452848124a1205c8fcccaa14cd8e3924761a
Thiago Arrais <thiago.arrais@gmail.com>
2017-07-04 23:46:16 +0000

    allow editing staging/prod environments on the ext's preferences page

--- browser-extension/anonydog/anonydog.js
+++ browser-extension/anonydog/anonydog.js
@@ -34,121 +34,135 @@ var waitFor = function(selector, operation) {
 
 };
 
+var augmentPullRequestPage = function(github_pr_button, env) {
+  github_pr_button.classList.remove("btn-primary");
+
+  var our_button_html = `
+    <button id="anonydog-pr" class="btn btn-primary" type="button">
+      Create anonymous pull request
+      <img
+        src="https://avatars3.githubusercontent.com/u/24738062?v=3&s=16"
+        style="margin-left: 3px"
+        width="16"
+        height="16" />
+    </button>
+  `;
+  github_pr_button.insertAdjacentHTML("beforebegin", our_button_html);
+
+  var deflect_pr = function() {
+    // TODO: we're assuming this meta tag contains repo_user/repo_name. Test for that
+    var repo_full_name = document.head.querySelector('meta[name="octolytics-dimension-repository_nwo"]').content,
+        parts = repo_full_name.split('/'),
+        repo_user = parts[0],
+        repo_name = parts[1];
+
+    request_fork(repo_user, repo_name, function() {
+      open_pr(env.bot_user, repo_name);
+    });
+  };
+
+  var headBranchNameFrom = function(url) {
+    //TODO: maybe use https://www.npmjs.com/package/query-string
+    var query = url.split("?")[1];
+    var entries = query.split("&").
+      map(function(pair_str) {
+        return pair_str.split("=").map(decodeURIComponent);
+      }).
+      map(function(entry_arr) {
+        return {
+          key: entry_arr[0],
+          value: entry_arr[1]
+        };
+      });
+
+    var head_entry_arr = entries.filter(function(entry) {
+      return entry.key == "head";
+    });
+
+    var head_entry = head_entry_arr[0];
+
+    return head_entry.value;
+  };
+
+  var request_fork = function(user, repo, callback) {
+    var request_fork_request = new XMLHttpRequest();
+
+    request_fork_request.onload = function() {
+      callback();
+    };
+
+    request_fork_request.open("POST", env.webapp_url + "/fork");
+
+    var post_data = new FormData();
+    post_data.append("user", user);
+    post_data.append("repo", repo);
+
+    request_fork_request.send(post_data);
+  };
+
+  var open_pr = function(dest_user, dest_repo_name) {
+    var compare_page_request = new XMLHttpRequest(),
+        compare_page_notfound = function() {
+          //the bot probably didn't create the anonymizing repo yet. wait a second and retry
+          //TODO: this isn't very clean. what should we do here? can we avoid getting to this state?
+          setTimeout(function() { open_pr(dest_user, dest_repo_name); }, 1000);
+        },
+        compare_page_ok = function() {
+          // reference to document assumes we're on the original pull request page
+          var orig_form = document.body.querySelector("#new_pull_request");
+          var dest_form = compare_page_request.responseXML.querySelector("#new_pull_request");
+
+          dest_form.elements.namedItem("pull_request[title]").value = orig_form.elements.namedItem("pull_request[title]").value;
+          dest_form.elements.namedItem("pull_request[body]").value = orig_form.elements.namedItem("pull_request[body]").value;
+
+          var form_data = new FormData(dest_form);
+
+          var create_pr_request = new XMLHttpRequest();
+
+          create_pr_request.onload = function() {
+            window.location.replace(create_pr_request.responseURL);
+          };
+
+          create_pr_request.open("POST", dest_form.action);
+          create_pr_request.send(form_data);
+        };
+
+    compare_page_request.onreadystatechange = function() {
+      if (compare_page_request.readyState === XMLHttpRequest.DONE && compare_page_request.status === 200) {
+        compare_page_ok();
+      } else if (compare_page_request.readyState === XMLHttpRequest.DONE && compare_page_request.status === 404) {
+        compare_page_notfound();
+      }
+    };
+
+    var orig_branch = headBranchNameFrom(document.body.querySelector("#new_pull_request").action),
+        compare_page_url = `https://github.com/${dest_user}/${dest_repo_name}/compare/master...${orig_branch}`;
+
+    compare_page_request.open("GET", compare_page_url);
+    compare_page_request.responseType = "document";
+
+    compare_page_request.send();
+  };
+
+  document.querySelector("#anonydog-pr").addEventListener("click", deflect_pr);
+};
+
 waitFor(
   "form#new_pull_request div.form-actions button[type=submit]",
   function(github_pr_button) {
-    github_pr_button.classList.remove("btn-primary");
-
-    var our_button_html = `
-      <button id="anonydog-pr" class="btn btn-primary" type="button">
-        Create anonymous pull request
-        <img
-          src="https://avatars3.githubusercontent.com/u/24738062?v=3&s=16"
-          style="margin-left: 3px"
-          width="16"
-          height="16" />
-      </button>
-    `;
-    github_pr_button.insertAdjacentHTML("beforebegin", our_button_html);
-
-    var deflect_pr = function() {
-      // TODO: we're assuming this meta tag contains repo_user/repo_name. Test for that
-      var repo_full_name = document.head.querySelector('meta[name="octolytics-dimension-repository_nwo"]').content,
-          parts = repo_full_name.split('/'),
-          repo_user = parts[0],
-          repo_name = parts[1];
-
-      console.log(`Will deflect PR to anonydog/${repo_name}`);
-
-      request_fork(repo_user, repo_name, function() {
-        //FIXME: "arraisbot" is hardcoded. need someway to switch between dev/staging/prod
-        open_pr("arraisbot", repo_name);
-      });
-    };
-
-    var headBranchNameFrom = function(url) {
-      //TODO: maybe use https://www.npmjs.com/package/query-string
-      var query = url.split("?")[1];
-      var entries = query.split("&").
-        map(function(pair_str) {
-          return pair_str.split("=").map(decodeURIComponent);
-        }).
-        map(function(entry_arr) {
-          return {
-            key: entry_arr[0],
-            value: entry_arr[1]
-          };
-        });
-
-      var head_entry_arr = entries.filter(function(entry) {
-        return entry.key == "head";
-      });
-
-      var head_entry = head_entry_arr[0];
-
-      return head_entry.value;
-    };
-
-    var request_fork = function(user, repo, callback) {
-      var request_fork_request = new XMLHttpRequest();
-
-      request_fork_request.onload = function() {
-        callback();
-      };
-
-      request_fork_request.open("POST", "http://webapp.dev.anonydog.org/fork"); //FIXME: hardcoded
-
-      var post_data = new FormData();
-      post_data.append("user", user);
-      post_data.append("repo", repo);
-
-      request_fork_request.send(post_data);
-    };
-
-    var open_pr = function(dest_user, dest_repo_name) {
-      var compare_page_request = new XMLHttpRequest(),
-          compare_page_notfound = function() {
-            //the bot probably didn't create the anonymizing repo yet. wait a second and retry
-            //TODO: this isn't very clean. what should we do here? can we avoid getting to this state?
-            setTimeout(function() { open_pr(dest_user, dest_repo_name); }, 1000);
-          },
-          compare_page_ok = function() {
-            // reference to document assumes we're on the original pull request page
-            var orig_form = document.body.querySelector("#new_pull_request");
-            var dest_form = compare_page_request.responseXML.querySelector("#new_pull_request");
-
-            dest_form.elements.namedItem("pull_request[title]").value = orig_form.elements.namedItem("pull_request[title]").value;
-            dest_form.elements.namedItem("pull_request[body]").value = orig_form.elements.namedItem("pull_request[body]").value;
-
-            var form_data = new FormData(dest_form);
-
-            var create_pr_request = new XMLHttpRequest();
-
-            create_pr_request.onload = function() {
-              window.location.replace(create_pr_request.responseURL);
-            };
-
-            create_pr_request.open("POST", dest_form.action);
-            create_pr_request.send(form_data);
-          };
-
-      compare_page_request.onreadystatechange = function() {
-        if (compare_page_request.readyState === XMLHttpRequest.DONE && compare_page_request.status === 200) {
-          compare_page_ok();
-        } else if (compare_page_request.readyState === XMLHttpRequest.DONE && compare_page_request.status === 404) {
-          compare_page_notfound();
+    browser.storage.local.get(
+      {
+        "env": {
+          bot_user: "anonydog",
+          webapp_url: "http://anonydog.org"
         }
-      };
-
-      var orig_branch = headBranchNameFrom(document.body.querySelector("#new_pull_request").action),
-          compare_page_url = `https://github.com/${dest_user}/${dest_repo_name}/compare/master...${orig_branch}`;
-
-      compare_page_request.open("GET", compare_page_url);
-      compare_page_request.responseType = "document";
-
-      compare_page_request.send();
-    };
-
-    document.querySelector("#anonydog-pr").addEventListener("click", deflect_pr);
+      }
+    ).
+    then(function(env) {
+      augmentPullRequestPage(github_pr_button, env);
+    },
+    function () {
+      console.log("anonydog could not retrieve env. panicking.")
+    });
   }
 );
