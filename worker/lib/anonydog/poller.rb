51851226c8335deb2c9e406f0db15dd6adc8229f
Thiago Arrais <thiago.arrais@gmail.com>
2017-07-22 11:59:54 +0000

    poller needs to be more resilient
    fixes #15

--- worker/lib/anonydog/poller.rb
+++ worker/lib/anonydog/poller.rb
@@ -25,24 +25,29 @@ module Anonydog
       poll_interval = 60
       last_modified_tag = ''
       while true do
-        threads = github_api.notifications(
-          :headers => {
-            'If-Modified-Since' => last_modified_tag
-          }
-        )
-        threads = (threads.instance_of? Array) ? threads : []
-        puts "Checked for notifications at #{Time.now}. Got #{threads.size} results"
+        begin
+          threads = github_api.notifications(
+            :headers => {
+              'If-Modified-Since' => last_modified_tag
+            }
+          )
+          threads = (threads.instance_of? Array) ? threads : []
+          puts "Checked for notifications at #{Time.now}. Got #{threads.size} results"
+  
+          http_response = github_api.last_response
 
-        http_response = github_api.last_response
+          poll_interval = (http_response.headers['X-Poll-Interval'] || poll_interval).to_i
+          last_modified_tag = http_response.headers['Last-Modified'] || last_modified_tag
 
-        poll_interval = (http_response.headers['X-Poll-Interval'] || poll_interval).to_i
-        last_modified_tag = http_response.headers['Last-Modified'] || last_modified_tag
-
-        threads.each do |thread|
-          if do_process_thread(thread) then
-            puts "Processed thread #{thread[:url]}"
-            github_api.mark_thread_as_read(thread[:id])
+          threads.each do |thread|
+            if do_process_thread(thread) then
+              puts "Processed thread #{thread[:url]}"
+              github_api.mark_thread_as_read(thread[:id])
+            end
           end
+        rescue => e
+          STDERR.puts e.message
+          STDERR.puts e.backtrace.join("\n")
         end
 
         sleep poll_interval
