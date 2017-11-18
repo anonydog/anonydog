dcfac0ba8350a3a2159fd693a48404d575d86fac
Thiago Arrais <thiago.arrais@gmail.com>
2017-11-18 06:50:09 -0300

    unify extensions: show configured values in the options page

--- browser-extension/anonydog/chrome-extension/options.js
+++ browser-extension/anonydog/chrome-extension/options.js
@@ -1,18 +1,17 @@
 function saveOptions(e) {
   e.preventDefault();
-  browser.storage.local.set({
-    env: {
-      bot_user: document.querySelector("#bot_user").value,
-      webapp_url: document.querySelector("#webapp_url").value
-    }
-  });
+  const env = {
+    bot_user: document.querySelector("#bot_user").value,
+    webapp_url: document.querySelector("#webapp_url").value
+  };
+  browser.storage.local.set({env});
 }
 
 function restoreOptions() {
 
   function setCurrentChoice(result) {
-    document.querySelector("#bot_user").value = result.bot_user || "anonydog";
-    document.querySelector("#webapp_url").value = result.webapp_url || "https://anonydog.org";
+    document.querySelector("#bot_user").value = result.env.bot_user || "anonydog";
+    document.querySelector("#webapp_url").value = result.env.webapp_url || "https://anonydog.org";
   }
 
   function onError(error) {
